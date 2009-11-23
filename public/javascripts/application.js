5f0660d0af7bdfd4e9231841507a00a4c264293a
James Adam <james@lazyatom.com>
2009-11-23 09:37:29 +0000

    Change the url hash and document title when going to a different section.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 
   $('#nav a').click(function () {
     $.scrollTo( $(this).attr("href"), 1000, {offset:-40} );
+    window.location.hash = $(this).text() + "_section";
+    document.title = $($(this).attr("href")).children("h2").text() + " :: Free Range";
     return false;
   })
 });
