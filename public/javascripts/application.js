0772829013570d450e944cfdb257d28933340262
James Adam <james@lazyatom.com>
2009-12-01 17:45:33 +0000

    Make the root snip 'Free Range', instead of 'start'.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
-    
-  document.title = main_title = "Free Range";
-
   $('#nav a, a.top').click(function () {
     $.scrollTo( $(this).attr("href"), 1000, {offset:-50} );
     window.location.hash = $(this).text() + "_section";
     document.title = $($(this).attr("href")).children("h2").text() + " :: " + main_title;
     return false;
   });  
-  
 });
