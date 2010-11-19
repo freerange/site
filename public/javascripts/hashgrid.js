e47ffc29c65ff3e2742d2374753f41bc09321264
Jason Cale <jase@jaseandtonic.com>
2010-11-19 16:35:56 +0000

    Style for blog article lists on the /blog page and also in the sidebar when viewing a post.
      * Made blog logo link to the '/blog'
      * Added silly way to specify the type of list element the kind of list dynasnip uses.
      * Updated hashgrid from github

--- public/javascripts/hashgrid.js
+++ public/javascripts/hashgrid.js
@@ -1,8 +1,11 @@
 /**
  * hashgrid (jQuery version)
  * http://github.com/dotjay/hashgrid
- * Version 3, 22 Feb 2010
- * By Jon Gibbins, accessibility.co.uk
+ * Version 5, 3 Nov 2010
+ * Written by Jon Gibbins, dotjay.co.uk, accessibility.co.uk
+ * Contibutors:
+ * Sean Coates, seancoates.com
+ * Phil Dokas, jetless.org
  *
  * // Using a basic #grid setup
  * var grid = new hashgrid();
@@ -22,16 +25,10 @@
  *     classPrefix: 'class',    // prefix for the grid classes
  *     cookiePrefix: 'mygrid'   // prefix for the cookie name
  * });
- *
  */
-
-$(document).ready(function() {
-
-	var grid = new hashgrid({
-		numberOfGrids: 1
-	});
-
-});
+if (typeof jQuery == "undefined") {
+	alert("Hashgrid: jQuery not loaded. Make sure it's linked to your pages.");
+}
 
 
 /**
@@ -75,8 +72,10 @@ var hashgrid = function(set) {
 	var overlayEl = $('<div></div>');
 	overlayEl
 		.attr('id', options.id)
-		.addClass(options.classPrefix + classNumber)
-		.css('display', 'none');
+		.css({
+			display: 'none',
+			'pointer-events': 'none'
+		});
 	$("body").prepend(overlayEl);
 	var overlay = $('#' + options.id);
 
@@ -88,20 +87,50 @@ var hashgrid = function(set) {
 	overlay.height(pageHeight);
 
 	// Add the first grid line so that we can measure it
-	overlay.append('<div class="horiz first-line">');
+	overlay.append('<div id="' + options.id + '-horiz" class="horiz first-line">');
+
+	// Position off-screen and display to calculate height
+	var top = overlay.css("top");
+	overlay.css({
+		top: "-999px",
+		display: "block"
+	});
 
 	// Calculate the number of grid lines needed
-	var overlayGridLines = overlay.children('.horiz'),
-		overlayGridLineHeight = parseFloat(overlayGridLines.css('height')) + parseFloat(overlayGridLines.css('border-bottom-width'));
+	var line = $('#' + options.id + '-horiz'),
+		lineHeight = line.outerHeight();
+
+	// Hide and reset top
+	overlay.css({
+		display: "none",
+		top: top
+	});
 
 	// Break on zero line height
-	if (overlayGridLineHeight <= 0) return true;
+	if (lineHeight <= 0) return true;
 
 	// Add the remaining grid lines
-	var i, numGridLines = Math.floor(pageHeight / overlayGridLineHeight);
+	var i, numGridLines = Math.floor(pageHeight / lineHeight);
 	for (i = numGridLines - 1; i >= 1; i--) {
 		overlay.append('<div class="horiz"></div>');
 	}
+
+	// vertical grid
+	overlay.append($('<div class="vert-container"></div>'));
+	var overlayVert = overlay.children('.vert-container');
+	var gridWidth = overlay.width();
+	overlayVert.css({width: gridWidth, position: 'absolute', top: 0});
+	overlayVert.append('<div class="vert first-line">&nbsp;</div>');
+
+	// 30 is an arbitrarily large number...
+	// can't calculate the margin width properly
+	for (i = 0; i < 30; i++) {
+		overlayVert.append('<div class="vert">&nbsp;</div>');
+	}
+
+	overlayVert.children()
+		.height(pageHeight)
+		.css({display: 'inline-block'});
 
 	// Check for saved state
 	var overlayCookie = readCookie(options.cookiePrefix + options.id);
@@ -119,8 +148,11 @@ var hashgrid = function(set) {
 		if (state[0] == '1') {
 			overlayOn = true;
 			sticky = true;
-			overlay.show();
+			showOverlay();
 		}
+	}
+	else {
+		overlay.addClass(options.classPrefix + classNumber);
 	}
 
 	// Keyboard controls
@@ -163,6 +195,18 @@ var hashgrid = function(set) {
 		createCookie(options.cookiePrefix + options.id, (sticky ? '1' : '0') + ',' + overlayZState + ',' + classNumber, 1);
 	}
 
+	function showOverlay() {
+		overlay.show();
+		overlayVert.css({width: overlay.width()});
+		// hide any vertical blocks that aren't at the top of the viewport
+		overlayVert.children('.vert').each(function () {
+			$(this).css('display','inline-block');
+			if ($(this).offset().top > 0) {
+				$(this).hide();
+			}
+		});
+	}
+
 	/**
 	 * Event handlers
 	 */
@@ -177,7 +221,7 @@ var hashgrid = function(set) {
 		switch(k) {
 			case options.showGridKey:
 				if (!overlayOn) {
-					overlay.show();
+					showOverlay();
 					overlayOn = true;
 				}
 				else if (sticky) {
@@ -215,6 +259,10 @@ var hashgrid = function(set) {
 					classNumber++;
 					if (classNumber > options.numberOfGrids) classNumber = 1;
 					overlay.addClass(options.classPrefix + classNumber);
+					showOverlay();
+					if (/webkit/.test( navigator.userAgent.toLowerCase() )) {
+						forceRepaint();
+					}
 					saveState();
 				}
 				break;
@@ -226,46 +274,67 @@ var hashgrid = function(set) {
 		if (!m) return true;
 		var k = getKey(e);
 		if (!k) return true;
-		switch(k) {
-			case options.showGridKey:
-				if (!sticky) {
-					overlay.hide();
-					overlayOn = false;
-				}
-				break;
+		if ((k == options.showGridKey) && !sticky) {
+			overlay.hide();
+			overlayOn = false;
 		}
+	}
+
+	/**
+	 * Cookie functions
+	 *
+	 * By Peter-Paul Koch:
+	 * http://www.quirksmode.org/js/cookies.html
+	 */
+	function createCookie(name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+	}
+
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+
+	function eraseCookie(name) {
+		createCookie(name,"",-1);
+	}
+
+	/**
+	 * Forces a repaint (because WebKit has issues)
+	 * http://www.sitepoint.com/forums/showthread.php?p=4538763
+	 * http://www.phpied.com/the-new-game-show-will-it-reflow/
+	 */
+	function forceRepaint() {
+		var ss = document.styleSheets[0];
+		try {
+			ss.addRule('.xxxxxx', 'position: relative');
+			ss.removeRule(ss.rules.length - 1);
+		} catch(e){}
 	}
 
 }
 
 
 /**
- * Cookie functions
- *
- * By Peter-Paul Koch:
- * http://www.quirksmode.org/js/cookies.html
+ * You can call hashgrid from your own code, but it's loaded here as
+ * an example for your convenience.
  */
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
+$(document).ready(function() {
 
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
+	var grid = new hashgrid({
+		numberOfGrids: 2
+	});
 
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
+});
