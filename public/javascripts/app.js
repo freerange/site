09ebfe73ffa7bc59876127ce19f8ca141ac74d8a
Jason Cale <jase@jaseandtonic.com>
2010-11-30 15:59:04 +0000

    Added bare bones for Chrono based layouts .. This is the javascript plugin which adds a class to the body depending on the day, and also has a few css styles that need to be fleshed out.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -203,8 +203,58 @@ b],f.body["scroll"+b],f.documentElement["scroll"+b],f.body["offset"+b],f.documen
   }  
 })(jQuery);
 
+// https://github.com/freerange/jQuery-ChronoClass
+;(function($) {
+  
+  Date.hourToPeriod = function (date) {
+    if (!date) { date = new Date() };
+    var period;
+    var hour = date.getHours();
+    if (hour > 5 && hour < 12) {
+      period = 'morning';
+    } else if (hour >= 12 && hour < 18) {
+      period = 'afternoon';
+    } else if (hour >= 18) {
+      period = 'evening';
+    };
+    return period;
+  }
+  
+  // What does the chronoClass plugin do?
+  $.fn.chronoClass = function(options) {
+    var opts = $.extend({}, $.fn.chronoClass.defaults, options);
+
+    return this.each(function() {
+      var $this = $(this);
+
+      // Support for the Metadata Plugin.
+      var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+
+      var now = new Date();
+      
+      if (o.dayOfWeek) {
+        var dayNames = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+        $this.addClass(dayNames[now.getDay()-1]);
+      };
+      
+      if (o.timeOfDay) {
+        $this.addClass(Date.hourToPeriod(now));
+      };
+
+    });
+
+  };
+
+  // default options
+  $.fn.chronoClass.defaults = {
+    dayOfWeek:true,
+    timeOfDay:false,
+  };
+
+})(jQuery);
 
 jQuery(document).ready(function($) {
   $('.blog_entry').find('p, blockquote, li').widowFix();
-  $('a[href*=fnref]').footNoteLink($('#blog_nav').height() || 0);  
+  $('a[href*=fnref]').footNoteLink($('#blog_nav').height() || 0);
+  $('body').chronoClass();
 });
