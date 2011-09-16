df68ea067c0300fa94d3a6df1b079b17c99a29fc
James Adam <james@lazyatom.com>
2011-09-16 17:54:03 +0100

    Copy LinkTo wholesale into l until I figure out why it breaks.

--- soups/dynasnips/l.rb
+++ soups/dynasnips/l.rb
@@ -1,1 +1,22 @@
-LinkTo
+require 'vanilla/dynasnip'
+
+class LinkTo < Dynasnip
+  usage %|
+The link_to dyna lets you create links between snips: 
+
+  {link_to blah} 
+
+would insert a link to the blah snip.|
+
+  def handle(name=nil, link_text=name, part=nil)
+    return usage if requesting_this_snip?
+    return "You must provide a snip name" unless name
+    if app.soup[name]
+      %{<a href="#{url_to(name, part)}">#{link_text}</a>}
+    else
+      %{<a class="missing" href="#{url_to(name, part)}">#{link_text}</a>}
+    end
+  end
+
+  self
+end
