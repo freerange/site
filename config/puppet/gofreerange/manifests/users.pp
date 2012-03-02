cb25b01de7b8927a95ebcb7ab286d4f59e2b5675
James Adam <james@lazyatom.com>
2012-03-02 11:48:38 +0000

    Remove Jase & Kalv.

--- config/puppet/gofreerange/manifests/users.pp
+++ config/puppet/gofreerange/manifests/users.pp
@@ -7,11 +7,6 @@ class gofreerange {
       key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA50zO1QYSl2lHaKyCshz0ph+H2t1SkUXPnDwn3Dn6ldQ6j46o+idhhN5+b8OnrmFXCAYhMDvaQnE/G6TpHal4x8Fq3W2ELkgkNDLQ5J8izphKHjsM+WyTrjskFViH4cNgpYkWrdUz3Yhxlx0EGqZZi57T/3Gf4hS/NspwYxhOWk5J+TptWAJQCI4sJR+Jw3TdTBqvkly8VX2dQYddPABnyDb0Ih8JEcXjngtGLsRi3N2DIq6YWvb2a3gYxpgkW7mgUOBFbOb9LMDmM2ILhYv5E8jhYbjsonH8f0NMxlWSQMIy2hoagm/SiRifyob3hA0HBqRDOJstwJkoEMkyv9z9aQ=="
     }
 
-    freerange::user {"jase@jasoncale.com":
-      user => "jasoncale",
-      key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA1O1lXgJcBenQ14N4S32J+m9q3HG1UY7gd+keXtFtZhMSvcByMNIcMREdXetC0XgeoTamybfK5+k8t18+oKX1E6zsUEzF2hYua52n8ZKH5Szsh/Gk7WLtsC6r7THC1yx0gihAt8FzFldzUAsFaYQIwBP514acQ1VqS+TDJltMSw8a31Z1t7LgeAWSPwj3A2meSxVdWDbAJ4n9tnGUTkCWZSm8AEUlRwSfU2lEPYvRtuKpcFllrK4f0kNBvKFWlXMCtU8MCHPJkq0DYxlf0YFOZooRpLMhIQojMmTKRwZI532miN2CF/Co2t8W7YBHhCwBmaG24Z5MybiByCaBwyBy8w=="
-    }
-
     freerange::user {"tom@popdog.net":
       user => "tomw",
       key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA7wy/hGcJZ1PFRCjs0UKIrALs+zm0d2+fVtHJqCdWTOOOaP3NRUEsi5Eb60vzuTkgEilcOhIT9jMUMCUzKHbqlG6EMhXFOsyaNDV30oyj9pHYeqUDCY52vb9GXTejFEFAy4bnlG+5N//B8nXiGuOjTWJdXuDhhvFSO1Cqp9doDQLfgnfP8pUmquVHYVN7aOIjIMlmnhXedLOAWhmfNaaA6IrbsqEqRAqe+YqRtB25Kqh4UCd8Fjd7dh98W32TrLtlh8qtb8e7U3W+lskpnbDubcptQKAdzbKDFpdpPQKGMtrCnPx3rnddZWH2dV36smm+IwwUbrjH3U/1F2ci8aCgFQ=="
@@ -20,12 +15,6 @@ class gofreerange {
     freerange::user {"james@lazyatom.com":
       user => "james",
       key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA2hsOv1phLQ6OsNMSLWMoXmV2q4qcaOZy6EDsX7+89WX1BQ/iCRckdvwhCc+KTYbIcCZA91JFhXtVwQ3tKDrJbdSYwTg3Y2a4dakbK87H63s7z5oIuM1KWXHGRV4RQGwqIgxNqkEwRUdbYkZ+Ct8X2W7bLTBkLA3zhOT7etxyg8WXRjGUdP6rpnAoJzVbnrn36SDKkWs+6yp5aNDhU4929En/8logAgBD3ByMAg9f/ndbVNA34ZuLWp6LYo2lZDrNxuAE4caseht+ovBWGiq84MmIMpVMuF0HuNNSh24CA8cF2DB7r+vYcmjHBz4VY7u4wkXBsPIbOjVwXVls5F7Hnw=="
-    }
-
-    freerange::user {"kalv@kalv.co.uk":
-      user => "kalv",
-      key => "AAAAB3NzaC1kc3MAAACBAIJ5jkXnd3XEcbSHVQ1RyCDfObE4efz1wfexP73/3RWEtIpmL9PsbVm57YMzpwU9ct94/RoHiNBIDr66gnfIdWnIVctjiZTZjTrolbWVSPgVq8RldNpVCDOJISKAoXAPWPTc9NuweTLxFIre9vVWY8Jkvz3AMrRubjcXUsc2NuKpAAAAFQCp8GdX6JUcnzjMvdcf39kUtHz6wwAAAIBwDchJvc87IGIK0p4ddCC0k59Tkre5xbUTEIUmyyUUaijPJQwR6gG86DMFNbrvkE0LJYy8lnVZ9mtAafHeC7zaiziYBUrBSQzVKf0cBjKy7WToCGE/FoAh28T3RomAamFeL31Fh7tXydHvf29UxME5MM0qOQaxSNmJ0ssPzY8jpwAAAIAeHG/4oUmOlwrqrOssK/jTNuYAdViVJxRMn7SzgHZ+tsjqoI5Gh1JvGGVMgI7zfKGZHDwyKjLY3+j0Yl29jtOLTgQmMzDFpOazScBNODehHKLDa1ILQ4zAiXsf9v4JO5vRGO0S9tuD+CvbBQp+r9CeLb2OxOIVfJMaAXkEONG9bw==",
-      key_type => "ssh-dss"
     }
 
     freerange::user {"james@floehopper.org":
