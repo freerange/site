f889565217fc9b6bc5e4ef34e09fa9880e3fb137
James Mead <james@floehopper.org>
2012-09-11 08:30:55 +0100

    Body class should handle case where no snip is found.
    This was causing 500s when they should have been 404s.

--- soups/dynasnips/body_class.rb
+++ soups/dynasnips/body_class.rb
@@ -2,6 +2,7 @@ require 'vanilla/dynasnip'
 
 class BodyClass < Dynasnip
   def handle(*args)
+    return "" unless app.request.snip
     case app.request.snip.kind
     when "blog", "draft"
       "blog"
