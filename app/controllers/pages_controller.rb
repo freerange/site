9fcacfdc92f356f5bb2d16d59cfa08914d8b0622
James Mead <james@floehopper.org>
2017-01-10 11:15:07 +0000

    Use a single soup incorporating both weeknotes & people
    Given that we can't really tell just from the request path what
    type of page we need to render, I think for now it's going to be
    simplest to use a single controller action and lookup all paths
    in a single soup much as the existing vanilla-rb app does.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,20 @@
 class PagesController < ApplicationController
   def show
     name = params.permit(:path)[:path]
-    @snip = weeknotes[name]
-    @author = people[@snip.author]
-    @soup = weeknotes
+    @snip = soup[name]
+    @author = soup[@snip.author]
     html = Kramdown::Document.new(@snip.content).to_html
     render html: html.html_safe, layout: 'blog'
   end
 
   private
 
-  def weeknotes
-    @weeknotes ||= Soup.new(Soup::Backends::FileBackend.new(Rails.root.join('soups/weeknotes')))
-  end
-
-  def people
-    @people ||= Soup.new(Soup::Backends::FileBackend.new(Rails.root.join('soups/people')))
+  def soup
+    @soup ||= begin
+      backends = %w(weeknotes people).map do |path|
+        Soup::Backends::FileBackend.new(Rails.root.join("soups/#{path}"))
+      end
+      Soup.new(Soup::Backends::MultiSoup.new(*backends))
+    end
   end
 end
