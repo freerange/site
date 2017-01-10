916c32960d9a30ac5c8c62fe244b84ee1a8d7125
James Mead <james@floehopper.org>
2017-01-10 10:31:52 +0000

    Incorporate blog-template markup into blog layout
    This provides things like the navigation side-bar, the author info,
    and Disqus commenting.
    
    I chose to convert blog-template.snip.haml into HTML and insert it
    into the existing blog layout. As far as I can see the only difference
    between this and show-and-tell-template.snip.haml is the "kind" filter
    on the of list links in the navigation side-bar. I hope that we can
    achieve the same effect in a single layout by using ERB tags.
    
    * Introduced `ApplicationHelper#article_date` which is an exact copy
      of `Vanilla::Renderers::Blog#article_date`.
    * Introduced `ApplicationHelper#list_of` which is almost an exact copy
      of `ListOf#handle`, but marking resulting HTML as safe and raising
      an exception if `kind` is not specified.
    * Introduced `ApplicationHelper#url_to` which is needed by `#list_of`.
      The implementation is based on bits of `UrlTo#handle` [1] and
      `Vanilla::Routing#url_to` [2]. I'm sure there are more Rails-y ways
      to do this, but it'll do for now.
    * Added a "people" soup so we can lookup author info.
    * Added a generic `@soup` so we can lookup snips for the nav side-bar
      in `ApplicationHelper#list_of`.
    * Use ERB tags in markup from blog-template.haml to interpolate bits of
      dynamic content originally gsub-ed in by
      `Vanilla::Renderers::Blog#process_text`.
    * Copied `soups/blog/disqus-commenting.snip` to
      `views/shared/_disqus_commenting.html`.
    
    [1]: https://github.com/lazyatom/vanilla-rb/blob/d632db587235722cab79d5dd07515e8252277ec5/pristine_app/soups/extras/url_to.rb#L4-L6
    [2]: https://github.com/lazyatom/vanilla-rb/blob/0ddf18f97738e2e8dfc973a198ffc82b339e011f/lib/vanilla/routing.rb#L4-L9

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,20 @@
 class PagesController < ApplicationController
   def show
-    path = Rails.root.join('soups/weeknotes')
-    backend = Soup::Backends::FileBackend.new(path)
-    soup = Soup.new(backend)
     name = params.permit(:path)[:path]
-    @snip = soup[name]
+    @snip = weeknotes[name]
+    @author = people[@snip.author]
+    @soup = weeknotes
     html = Kramdown::Document.new(@snip.content).to_html
     render html: html.html_safe, layout: 'blog'
+  end
+
+  private
+
+  def weeknotes
+    @weeknotes ||= Soup.new(Soup::Backends::FileBackend.new(Rails.root.join('soups/weeknotes')))
+  end
+
+  def people
+    @people ||= Soup.new(Soup::Backends::FileBackend.new(Rails.root.join('soups/people')))
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,45 @@
 module ApplicationHelper
+  def article_date(time)
+    suffix = case time.day
+    when 1, 21, 31
+      "st"
+    when 2, 22
+      "nd"
+    when 3, 23
+      "rd"
+    else
+      "th"
+    end
+
+    time.strftime("%A %d#{suffix} %B, %Y")
+  end
+
+  def list_of(options={})
+    options = {:element => 'ul'}.merge(options)
+
+    if kind = options[:kind]
+      limit = options[:limit]
+      el = options[:element]
+      kinds = kind.split(',')
+
+      entries = kinds.flat_map { |k| @soup[:kind => k] }.reject { |s| s.draft }.sort_by { |e| e.created_at }.reverse
+      entries = entries[0..limit.to_i] unless limit == nil
+
+      value = entries.length + 1
+
+      html = "<#{el} class='" + kinds.first + "_list' reversed>" + entries.map do |entry|
+        title = entry.content.split("\n").first
+        url = url_to(entry.name)
+        value -= 1
+        %{<li value="#{value}"><a href="#{url}">#{title}</a></li>}
+      end.join + "</#{el}>"
+      html.html_safe
+    else
+      raise 'No kind specified'
+    end
+  end
+
+  def url_to(snip_name)
+    @soup[snip_name] ? "/#{snip_name}" : "[Snip '#{snip_name}' not found]"
+  end
 end
