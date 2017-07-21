6bf5651bd73e153de25f9234585514437f7ccabe
James Mead <james@floehopper.org>
2017-07-21 15:54:39 +0100

    Change exciting.io redirects to use HTTPS

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   get '/feed.xml', to: 'pages#feed'
   get '/sitemap.xml', to: 'pages#sitemap'
 
-  get '/hello-printer', to: redirect('http://exciting.io/2012/04/12/hello-printer')
-  get '/printer-questions', to: redirect('http://exciting.io/2012/05/01/printer-questions')
-  get '/printer-kit', to: redirect('http://exciting.io/printer')
-  get '/printer', to: redirect('http://exciting.io/printer')
-  get '/harmonia', to: redirect('http://exciting.io/harmonia')
-  get '/heylist', to: redirect('http://exciting.io/heylist')
-  get '/say-hello-to-heylist', to: redirect('http://exciting.io/2013/03/25/introducing-heylist')
+  get '/hello-printer', to: redirect('https://exciting.io/2012/04/12/hello-printer')
+  get '/printer-questions', to: redirect('https://exciting.io/2012/05/01/printer-questions')
+  get '/printer-kit', to: redirect('https://exciting.io/printer')
+  get '/printer', to: redirect('https://exciting.io/printer')
+  get '/harmonia', to: redirect('https://exciting.io/harmonia')
+  get '/heylist', to: redirect('https://exciting.io/heylist')
+  get '/say-hello-to-heylist', to: redirect('https://exciting.io/2013/03/25/introducing-heylist')
 
   get '*path', to: 'pages#show'
 end
