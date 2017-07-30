1c98670b538272548dd4251a4d29e4c4b23d8cfb
James Mead <james@floehopper.org>
2017-07-30 16:41:45 +0100

    Rename Company#weeks_since_incorporation -> week_number_for

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,14 +5,14 @@ class Company
 
   GoFreeRange = new(incorporation_date: Date.parse('2009-01-13'))
 
-  def weeks_since_incorporation(date)
+  def week_number_for(date)
     monday_beginning_this_week = date.monday
     days_since_incorporation = monday_beginning_this_week - monday_beginning_the_week_of_incorporation
     days_since_incorporation / 7.0
   end
 
   def week_numbers_and_dates
-    current_week_number = weeks_since_incorporation(Date.today).to_i
+    current_week_number = week_number_for(Date.today).to_i
 
     (0..current_week_number).map do |week_number|
       monday_in_week = monday_beginning_the_week_of_incorporation + (week_number * 7)
