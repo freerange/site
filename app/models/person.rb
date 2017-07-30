937f68c27333e11abb1c198967a0d3acc1288909
James Mead <james@floehopper.org>
2017-07-30 22:37:26 +0100

    Fail fast if person snip not found

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
-class Person
+class Person < Base
   USERNAMES_VS_PERSON_NAMES = {
     'jamesmead' => 'james-mead',
     'chrisroos' => 'chris-roos',
@@ -6,11 +6,12 @@ class Person
   }
 
   def self.current_name
-    USERNAMES_VS_PERSON_NAMES.fetch(current_username)
+    person_name = USERNAMES_VS_PERSON_NAMES.fetch(current_username)
+    find(person_name).name
   end
 
   def self.find(name)
-    soup[name]
+    soup[name] || raise(NotFoundError.new("Person snip not found: #{name}"))
   end
 
   def self.create(attributes)
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -10,13 +10,19 @@ RSpec.describe Person, type: :model do
 
   describe '.current_name' do
     it 'returns snip name associated with current unix user' do
+      described_class.create(name: 'james-mead')
       allow(described_class).to receive(:current_username).and_return('jamesmead')
       expect(described_class.current_name).to eq('james-mead')
     end
 
     it 'raises exception if no person name found for current unix user' do
+      described_class.create(name: 'james-mead')
       allow(described_class).to receive(:current_username).and_return('unknown')
       expect { described_class.current_name }.to raise_error(KeyError)
+    end
+
+    it 'raises not found error if no snip exists with the specified name' do
+      expect { described_class.current_name }.to raise_error(Base::NotFoundError)
     end
   end
 
@@ -24,6 +30,10 @@ RSpec.describe Person, type: :model do
     it 'returns snip with specified name' do
       snip = described_class.create(name: 'snip-name')
       expect(described_class.find('snip-name')).to eq(snip)
+    end
+
+    it 'raises not found error if no snip exists with the specified name' do
+      expect { described_class.find('snip-name') }.to raise_error(Base::NotFoundError)
     end
   end
 
