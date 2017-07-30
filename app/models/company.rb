c504699e58a6ac4a816c98f84739304ec610c1af
James Mead <james@floehopper.org>
2017-07-30 16:48:24 +0100

    Inline methods & variables to simplify Company methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,8 +6,7 @@ class Company
   GoFreeRange = new(incorporation_date: Date.parse('2009-01-13'))
 
   def week_number_for(date)
-    monday_beginning_this_week = date.monday
-    days_since_incorporation = monday_beginning_this_week - monday_beginning_the_week_of_incorporation
+    days_since_incorporation = date.monday - @incorporation_date.monday
     (days_since_incorporation / 7.0).to_i
   end
 
@@ -15,14 +14,8 @@ class Company
     current_week_number = week_number_for(Date.today)
 
     (0..current_week_number).map do |week_number|
-      monday_in_week = monday_beginning_the_week_of_incorporation + (week_number * 7)
+      monday_in_week = @incorporation_date.monday + (week_number * 7)
       [week_number, monday_in_week]
     end
-  end
-
-  private
-
-  def monday_beginning_the_week_of_incorporation
-    @incorporation_date.monday
   end
 end
