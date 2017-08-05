2fc79fa0afcd01778222226a33f78fea1cdbc097
James Mead <james@floehopper.org>
2017-08-05 15:04:49 +0100

    Revert "Remove protect_from_forgery"
    This reverts commit b04953c5ce7736d397946a347819b28893f26415.
    
    @chrisroos suggested that if this isn't doing any harm, it might be
    safer to leave it in place in case we start handling POST requests in
    the future.
    
    Closes #76.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   rescue_from 'Base::NotFoundError' do |exception|
     render plain: exception.message, status: :not_found
   end
