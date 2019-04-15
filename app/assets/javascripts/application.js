2d141a486c9323e6547a6cb989c283dbfc94c820
Chris Roos <chris@seagul.co.uk>
2019-04-15 20:36:41 +0100

    Upgrade to jQuery 3
    The addChronoClassToBody.js and footNoteLink.js continue to work
    correctly with this version.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require_tree .
