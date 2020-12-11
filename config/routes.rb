1244e66ece499a2dc8d3500959b6204233e104ac
James Mead <james@floehopper.org>
2020-12-11 10:50:28 +0000

    Reinstate routes after being overwritten in Rails upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: { subdomain: 'www' }
+
+  root to: 'pages#show'
+  get '/feed.xml', to: 'pages#feed', format: 'atom'
+  get '/GoFreeRangeBlog', to: 'pages#feed', format: 'atom'
+  get '/sitemap.xml', to: 'pages#sitemap'
+
+  namespace :twilio do
+    defaults format: :xml do
+      resource :conference, only: %i(create) do
+        member do
+          get :auth
+          get :auth_fail
+        end
+      end
+    end
+    resources :assets, only: %(show)
+  end
+
+  get '/hello-printer', to: redirect('https://exciting.io/2012/04/12/hello-printer')
+  get '/printer-questions', to: redirect('https://exciting.io/2012/05/01/printer-questions')
+  get '/printer-kit', to: redirect('https://exciting.io/printer')
+  get '/printer', to: redirect('https://exciting.io/printer')
+  get '/harmonia', to: redirect('https://exciting.io/harmonia')
+  get '/heylist', to: redirect('https://exciting.io/heylist')
+  get '/say-hello-to-heylist', to: redirect('https://exciting.io/2013/03/25/introducing-heylist')
+
+  get '/recap/docs', to: redirect('https://freerange.github.io/recap')
+  get '/recap/docs/*path', to: redirect('https://freerange.github.io/recap/%{path}'), format: false
+
+  get '/mocha/docs', to: redirect('https://mocha.jamesmead.org')
+  get '/mocha/docs/*path', to: redirect('https://mocha.jamesmead.org/%{path}'), format: false
+
+  get '*path', to: 'pages#show'
 end
