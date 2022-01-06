50af1a93adc738991f75ae7b0fcec6645bae845c
James Mead <james@floehopper.org>
2022-01-06 21:49:47 +0000

    Update ruby from v2.6.6 -> 2.7.4
    My actual motivation for doing this was to use a ruby version definitely
    supported by the latest nixpkgs, but it's worth updating anyway.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.6.6'
+ruby '2.7.4'
 
 gem 'rails', '~> 6.1'
 gem 'soup', '~> 1.0'
--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with (import <nixpkgs> {});
 let
   env = bundlerEnv {
     name = "site-bundler-env";
-    ruby = ruby_2_5;
+    ruby = ruby_2_7;
     gemdir  = ./.;
   };
 in mkShell {
