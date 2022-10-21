05cc2543c00eea56536ed8f400619baf866441a4
Chris Roos <chris@seagul.co.uk>
2022-10-21 17:05:01 +0100

    Update Rails config
    By running `rails app:update` and accepting all the changes.
    
    I will reinstate some desirable behaviour in subsequent commits.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ require "action_controller/railtie"
 # require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module Site
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
@@ -30,5 +30,8 @@ module Site
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
