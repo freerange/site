5a79d3345b0c9cfc92bc5fc8c3d8e3ff6246a826
Chris Roos <chris@seagul.co.uk>
2022-10-24 16:48:45 +0100

    Load Rails 7 defaults
    And remove the now redundant:
    
    - `config.active_support.cache_format_version = 7.0`
    - new_framework_defaults_7_0.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module Site
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -34,7 +34,5 @@ module Site
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.active_support.cache_format_version = 7.0
   end
 end
