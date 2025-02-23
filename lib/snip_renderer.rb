3e4b3e6303aac92e8c0169f5b37e4f04d9c10dac
James Mead <james@floehopper.org>
2025-02-23 17:12:40 +0000

    Require explicit enabling of ERB in snips
    This defaults to *not* applying ERB rendering unless it is explicitly
    enabled using an `erb` attribute in the snip.
    
    This means we'll need to update a bunch of snips to add `:erb: true`,
    but I plan to do that in a separate commit to make things clearer.

--- lib/snip_renderer.rb
+++ lib/snip_renderer.rb
@@ -1,11 +1,18 @@
 class SnipRenderer
   def render(snip, context)
-    content = ERB.new(snip.content).result(context)
-    case snip.extension
-    when 'markdown'
-      Kramdown::Document.new(content, input: 'GFM', syntax_highlighter: 'rouge').to_html
-    else
-      content
+    formats = []
+    formats << 'erb' if snip.erb
+    formats << 'markdown' if snip.extension == 'markdown'
+
+    formats.inject(snip.content) do |content, format|
+      case format
+      when 'erb'
+        ERB.new(content).result(context)
+      when 'markdown'
+        Kramdown::Document.new(content, input: 'GFM', syntax_highlighter: 'rouge').to_html
+      else
+        content
+      end
     end
   end
 end
