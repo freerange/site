c35f9af27c709ec56551e2e6f838d3c020aec09c
James Mead <james@floehopper.org>
2025-11-28 15:50:31 +0000

    Load defaults for Rails v7.1
    I've clicked around the site and it all seems to work OK.
    
    So we can load the defaults for v7.1 and delete the new framework
    defaults initializer.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module Site
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -39,9 +39,5 @@ module Site
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # Defaults for Rails v7.1
-    config.add_autoload_paths_to_load_path = false
-    config.active_support.cache_format_version = 7.1
   end
 end
