046217cfe244a27bd81aa2bd5dc591d19288df7e
James Mead <james@floehopper.org>
2026-04-09 18:13:10 +0100

    Fix link to CSS Naked Day website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,7 +64,7 @@ module ApplicationHelper
   # CSS Naked Day is supposed to last for 48 hours because it's April 9th
   # somewhere in the world for that period of time. This is the best I've
   # come up with based on the furthest UTC offsets on April 9th.
-  # https://css-naked-day.github.io
+  # https://css-naked-day.org/
   # https://en.wikipedia.org/wiki/List_of_UTC_time_offsets
   def css_naked_day?
     css_naked_day = Date.parse('April 9').to_time
