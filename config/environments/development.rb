f2acc4dc0395967f7ae68ff49b7476498e6dc762
James Mead <james.mead@gofreerange.com>
2026-04-26 22:32:36 +0100

    Enable activerecord with postgresql
    I ran the following command from the parent directory:
    
        bundle exec rails new --minimal --skip-git --skip-rubocop \
          --skip-brakeman --skip-ci --skip-docker --skip-test --force \
          --database=postgresql site
    
    And then manually accepted or rejected the relevant changes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,12 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
