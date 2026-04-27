d316baada74e208174d6ad4a548610319bf4954f
James Mead <james.mead@gofreerange.com>
2026-04-27 12:50:10 +0100

    Enable transactional fixtures in specs
    So that database operations will be rolled back after every example to provide
    isolation between them.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.use_transactional_fixtures = true
 end
 
 require 'capybara/rspec'
