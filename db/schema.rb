487c6e9eefcee7d07860a216e9a5b6f524968c24
James Mead <james.mead@gofreerange.com>
2026-05-02 20:32:29 +0100

    Create shortener_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_27_113313) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_02_162241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +20,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_27_113313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["key"], name: "index_shortener_mappings_on_key", unique: true
+  end
+
+  create_table "shortener_users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_shortener_users_on_email", unique: true
   end
 end
